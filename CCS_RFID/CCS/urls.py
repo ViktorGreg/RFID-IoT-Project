a0f62617ef1b78c0a44866300507c2b291a2ea93
ViktorGreg <alfahadadian04@gmail.com>
2026-02-18 23:13:12 +0800

    Created Base.html
    Created a base.html for smoother code for the next dev

--- CCS_RFID/CCS/urls.py
+++ CCS_RFID/CCS/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
+    path('', views.login, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('attendance/', views.attendance, name='attendance'),
+    path('classes/', views.classes, name='classes'),
+    path('activity/', views.activity, name='activity'),
+    path('schedule/', views.schedule, name='schedule'),
 ]
--- CCS_RFID/CCS/views.py
+++ CCS_RFID/CCS/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html', {})
+def login(request):
+    return render(request, 'login.html', {})
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def attendance(request):
+    return render(request, 'attendance.html')  # Create this template
+
+def classes(request):
+    return render(request, 'classes.html')  # Create this template
+
+def activity(request):
+    return render(request, 'activity.html')  # Create this template
+
+def schedule(request):
+    return render(request, 'schedule.html')  # Create this template
